9edb54ed5e037fa810dce10fd6ac4e2fa8d31e38
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-19 14:57:16 -0300

    começando a inplementação de um input para trocar cor dos times

--- src/App.js
+++ src/App.js
@@ -193,7 +193,7 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
-  function detelarColaborador(){
+  function deletarColaborador(){
     console.log('deletando');
   }
 
@@ -203,7 +203,7 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
-        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} aoDeletar={detelarColaborador} />)}
+        {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} aoDeletar={deletarColaborador} />)}
       </section>
       <Rodape />
     </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,11 +5,12 @@ const Time = ({ time, colaboradores, aoDeletar }) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input value={time.corSecundaria} type='color' className='input-cor' />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
                     
-                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={time.aoDeletar}/>
+                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/>
                 })}
             </div>
         </section>
