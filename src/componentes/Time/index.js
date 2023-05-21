47eab13e213174be583840ab9e71d4bb83327afe
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-20 22:44:29 -0300

    implementando o botão de favorito

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,7 +2,7 @@ import Colaborador from '../Colaborador';
 import './time.css';
 import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ time, colaboradores, aoDeletar, mudarCor}) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar}) => {
     return (
 
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba (time.cor, '0.6') }}>
@@ -11,7 +11,7 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor}) => {
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
                     
-                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar}/>
+                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} aoFavoritar={aoFavoritar}/>
                 })}
             </div>
         </section>
